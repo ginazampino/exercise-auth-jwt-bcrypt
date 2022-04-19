59e73e5e9375fe5c1895a417a83217741310e92d
Gina Zampino <zampino.gina@gmail.com>
2022-04-18 21:43:06 -0400

    Updated middleware.

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const history = require('connect-history-api-fallback');
 
 module.exports = function(app) {
@@ -7,5 +9,12 @@ module.exports = function(app) {
     app.use(express.static(path.resolve(__dirname, '../../build')));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
+    
     require('../routes')(app);
 };
