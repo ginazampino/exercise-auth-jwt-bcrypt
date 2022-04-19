b6bdf2dd92a813d37db176eee8c549f5ed0f1fd4
Gina Zampino <zampino.gina@gmail.com>
2022-04-18 21:41:56 -0400

    Initialized database configuration.

---
+++ server/database/index.js
@@ -0,0 +1,19 @@
+const { Model } = require('objection');
+const Knex = require('knex');
+
+require('dotenv').config();
+
+const knex = Knex({
+    client: 'mysql2',
+    connection: {
+        host : 'localhost',
+        user : process.env.DB_USERNAME,
+        password : process.env.DB_PASSWORD,
+        database : 'petzhub'
+    },
+    pool: { min: 0, max: 5 }
+})
+
+Model.knex(knex);
+
+module.exports = knex;
