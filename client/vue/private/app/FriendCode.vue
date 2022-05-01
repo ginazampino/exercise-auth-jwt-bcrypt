92ba2e517a401c4b707b5e29a62335dd09f46d63
Gina Zampino <zampino.gina@gmail.com>
2022-04-30 22:04:12 -0400

    Added line breaks to some functions.

--- client/vue/private/app/FriendCode.vue
+++ client/vue/private/app/FriendCode.vue
@@ -15,8 +15,14 @@
         },
 
         async mounted() {
-            const session = await axios.get('/api/get/cookie').then((result) => { return result; }).catch((error) => { throw error; });
-            const profile = await axios.get(`/api/get/user/${session.data}/private`).then((result) => { return result; }).catch((error) => { throw error; });
+            const session = await axios.get('/api/get/cookie')
+                .then((result) => { return result; })
+                .catch((error) => { throw error; });
+                
+            const profile = await axios.get(`/api/get/user/${session.data}/private`)
+                .then((result) => { return result; })
+                .catch((error) => { throw error; });
+
             this.friendCode = profile.data.friendCode;
         }
     };
