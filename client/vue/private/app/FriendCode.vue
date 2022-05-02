06849e977330fe17492530211e089b952922872f
Gina Zampino <zampino.gina@gmail.com>
2022-05-02 08:55:09 -0400

    Added code page content.

--- client/vue/private/app/FriendCode.vue
+++ client/vue/private/app/FriendCode.vue
@@ -1,7 +1,30 @@
 <template>
-    <code>
-        {{ friendCode }}
-    </code>
+    <main>
+        <div class="content">
+            <section>
+                <h1 class="red">
+                    <i class="fa-solid fa-gift blue"></i> Invite a friend
+                </h1>
+                <div class="divider yellow"></div>
+            </section>
+            <section>
+                <div class="code-container" v-on:click="copyText" aria-label="Click to add to clipboard">
+                    <h2>
+                        Your friend code
+                    </h2>
+                    <code>
+                        {{ friendCode }} <i class="fa-solid fa-floppy-disk" alt="Floppy disk icon"></i>
+                    </code>
+                </div>
+            </section>
+            <section>
+                    <div class="divider yellow"></div>
+                    <footer>
+                        I have no idea what I'm doing.
+                    </footer>
+            </section>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -12,6 +35,12 @@
             return {
                 friendCode: null
             };
+        },
+
+        methods: {
+            copyText() {
+                navigator.clipboard.writeText(this.friendCode);
+            }
         },
 
         async mounted() {
