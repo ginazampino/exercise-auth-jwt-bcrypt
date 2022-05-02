3f81728c2c2188353274137d87a92701c2327019
Gina Zampino <zampino.gina@gmail.com>
2022-05-02 08:54:54 -0400

    Increased scale of 404 layout.

--- client/vue/Error404.vue
+++ client/vue/Error404.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="error">
-        <img src="../img/spray.gif">
+        <img src="../img/spray.gif" alt="Spray bottle">
         <h1>
             404 Not Found-it
         </h1>
-        <router-link to="/">Go back</router-link>
+        <router-link to="/home" aria-label="Return to home page">Go home</router-link>
     </div>
 </template>
